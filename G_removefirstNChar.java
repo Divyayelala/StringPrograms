0bb696692406827d324ba510ba3fe9661d8bf3e4
Divyayelala <divyayelala2404@gmail.com>
2017-08-14 20:25:40 +0530

    removing first n elements from string

---
+++ G_removefirstNChar.java
@@ -0,0 +1,15 @@
+
+public class G_removefirstNChar {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String s1 = "hi how are you";
+		int n = 4;
+		for(int i= n;i < s1.length();i++)
+		{
+			System.out.print(s1.charAt(i));
+		}
+
+	}
+
+}
